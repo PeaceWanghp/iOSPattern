caffa61ea745a3839a70dff89cc723bfb647c502
wangheping <wangheping@b.360.cn>
2020-11-08 15:30:52 +0800

    add Mediator Pattern 中介者模式

---
+++ iOSPattern/Pattern/Mediator/PMediatorController.h
@@ -0,0 +1,16 @@
+//
+//  PMediatorController.h
+//  iOSPattern
+//
+//  Created by Peace on 11/8/20.
+//
+
+#import "BPresentController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PMediatorController : BPresentController
+
+@end
+
+NS_ASSUME_NONNULL_END
